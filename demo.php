dd480a9857808b6e089372799622d666a56928b4
pengpaoge@outlook.com <pengpaoge@outlook.com>
2020-01-13 16:31:38 +0800

    第一次提交

---
+++ demo.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * This is Demo
+ * @author PengJianglin<pengapoge@outlook.com>
+ * @date   2019-01-20
+ */
+
+require_once './vendor/autoload.php';
+use Bruce\First\Tools;
+
+$tool = new Tools();
+$tool->hello();
---
+++ src/Bruce/First/Tools.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Bruce\First;
+
+/**
+ * 第一个工具
+ * @author PengJianglin<pengpaoge@outlook.com>
+ * @date   2020-01-21
+ */
+class Tools {
+    public function __construct() {
+        
+    }
+
+    public function hello() {
+        echo "Hello world!\n";
+    }
+}
